faba1a0cc50f647c61f44fc65ed8c273256ac088
tomelsj <tomelssjo@gmail.com>
2018-01-25 16:45:41 +0800

    Sprite

--- client/src/js/3dsetup.js
+++ client/src/js/3dsetup.js
@@ -1,9 +1,17 @@
 var gl;
+var canvasmain;
+var neheTexture;
 
 import { mat4 } from "gl-matrix";
 
+/**
+ * Modified by Tom Elssjö
+ * Copyright (c) 2018 Tom Elssjö
+ * Copyright (C) 2009-2013 Giles Thomas
+ */
+
 function setup3d() {
-    var canvasmain = document.getElementById("gamemain");
+    canvasmain = document.getElementById("gamemain");
 
     let variants = [ "webgl", "exprimental-webgl", "webkit-3d", "moz-webgl" ];
     for (let vari of variants) {
@@ -17,13 +25,15 @@ function setup3d() {
         }
     }
 
-    gl.viewportWidth = canvasmain.width;
-    gl.viewportHeight = canvasmain.height;
-
     initShaders();
     initBuffers();
+    initTexture();
+    resizeCanvas();
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LESS);
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
     drawScene();
 }
 
@@ -62,18 +72,47 @@ var shaderProgram;
 function initShaders() {
     var fragmentShader = getShader(gl, "shader-fs");
     var vertexShader = getShader(gl, "shader-vs");
+
     shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
+
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
         alert("Could not initialise shaders");
     }
+
     gl.useProgram(shaderProgram);
+
     shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
     gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+    shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
+    gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
+
     shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
     shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+    shaderProgram.samplerUniform = gl.getUniformLocation(shaderProgram, "uSampler");
+}
+
+function handleLoadedTexture(texture) {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    drawScene();
+}
+
+function initTexture() {
+    neheTexture = gl.createTexture();
+    neheTexture.image = new Image();
+    neheTexture.image.onload = () => {
+        handleLoadedTexture(neheTexture);
+    };
+    neheTexture.image.src = "img/dude.png";
 }
 
 var mvMatrix = mat4.create();
@@ -84,54 +123,87 @@ function setMatrixUniforms() {
     gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 }
 
-var triangleVertexPositionBuffer;
 var squareVertexPositionBuffer;
+var squareVertexTextureCoordBuffer;
 
 function initBuffers() {
-    triangleVertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-    var vertices = [
-        0.0,  1.0,  0.0,
-        -1.0, -1.0,  0.0,
-        1.0, -1.0,  0.0
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    triangleVertexPositionBuffer.itemSize = 3;
-    triangleVertexPositionBuffer.numItems = 3;
     squareVertexPositionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-    vertices = [
+
+    const vertices = [
         1.0,  1.0,  0.0,
-        -1.0,  1.0,  0.0,
-        1.0, -1.0,  0.0,
+        1.0,  -1.0,  0.0,
+        -1.0, 1.0,  0.0,
         -1.0, -1.0,  0.0
     ];
+
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     squareVertexPositionBuffer.itemSize = 3;
     squareVertexPositionBuffer.numItems = 4;
+
+    squareVertexTextureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexTextureCoordBuffer);
+
+    const textureCoordinates = [
+        // Front
+        1.0,  1.0,
+        1.0, 0.0,
+        0.0,  1.0,
+        0.0,  0.0
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates),
+        gl.STATIC_DRAW);
+    squareVertexTextureCoordBuffer.itemSize = 2;
+    squareVertexTextureCoordBuffer.numItems = 4;
 }
 
 function drawScene() {
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    // Update: mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix); mat4.perspective() API has changed.
     mat4.perspective(pMatrix, 45.0, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
     mat4.identity(mvMatrix);
-    // Update: mat4.translate(mvMatrix, [-1.5, 0.0, -7.0]); mat4.translate() API has changed to mat4.translate(out, a, v)
-    // where out is the receiving matrix, a is the matrix to translate, and v is the vector to translate by. z altered to
-    // approximate original scene.
+
     mat4.translate(mvMatrix, mvMatrix, [ -1.5, 0.0, -5.4 ]);
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
-        triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-    setMatrixUniforms();
-    gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
-    mat4.translate(mvMatrix, mvMatrix, [ 3.0, 0.0, 0.0 ]);
+
     gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
         squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexTextureCoordBuffer);
+    gl.vertexAttribPointer(shaderProgram.textureCoordAttribute,
+        squareVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    // Tell WebGL we want to affect texture unit 0
+    gl.activeTexture(gl.TEXTURE0);
+    // Bind the texture to texture unit 0
+    gl.bindTexture(gl.TEXTURE_2D, neheTexture);
+    // Tell the shader we bound the texture to texture unit 0
+    gl.uniform1i(shaderProgram.samplerUniform, 0);
+
     setMatrixUniforms();
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
 }
+
+/**
+ * https://stackoverflow.com/questions/13870677/resize-viewport-canvas-according-to-browser-window-size
+ */
+function resizeCanvas() {
+    let width = canvasmain.clientWidth;
+    let height = canvasmain.clientHeight;
+    if (canvasmain.width != width ||
+        canvasmain.height != height) {
+        canvasmain.width = width;
+        canvasmain.height = height;
+
+        gl.viewportWidth = canvasmain.width;
+        gl.viewportHeight = canvasmain.height;
+
+        // in this case just render when the window is resized.
+        drawScene();
+    }
+}
+
+window.addEventListener("resize", resizeCanvas);
 
 export { setup3d };
